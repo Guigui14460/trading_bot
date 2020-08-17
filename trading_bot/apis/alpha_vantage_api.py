9e1a06a5a93db6e15251b824e158dae780549417
ChivaSdev <melvinlucchini357@gmail.com>
2020-08-17 19:17:47 +0200

    Fonction qui permet d'afficher les données et de changer le nom des colonnes

--- trading_bot/apis/alpha_vantage_api.py
+++ trading_bot/apis/alpha_vantage_api.py
@@ -1,12 +1,24 @@
-from .base_api import BaseAPI
+from trading_bot.apis.base_api import BaseAPI
 from trading_bot.settings import ALPHA_VANTAGE_API_KEY
-import alpha_vantage 
+from alpha_vantage.timeseries import TimeSeries
 
 class AlphaVantage(BaseAPI):
 
     def __init__(self):
         BaseAPI.__init__(self, None)
 
-    def load_data(self, endpoint, start_date, end_date):
-        return super().load_data(endpoint, start_date, end_date)
+    def load_data(self, start_date, end_date, symbol: str) -> dict:
+        ts = TimeSeries(key="ALPHA_VANTAGE_API_KEY", output_format="pandas")
+        data, meta_data = ts.get_intraday(symbol, interval="1min")
+        self.df = data
+        dataDict = {"1. open": "Open", 
+        "2. high": "High", 
+        "3. low": "Low", 
+        "4. close": "Close", 
+        "5. volume": "Volume"
+}
+        self.rename_column_name(dataDict)
+        return data
+
+
 
