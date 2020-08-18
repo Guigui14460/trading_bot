34b42cc74f6eeb3e46f0d87cdbed173ccbf7a84a
Guigui14460 <letgui2@gmail.com>
2020-08-18 11:22:16 +0200

    Remove useless setting

--- trading_bot/settings.py
+++ trading_bot/settings.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY") or "demo"
+ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
 
 DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
