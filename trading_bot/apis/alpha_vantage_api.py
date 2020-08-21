37723f147d19f9b7764ee6e2e87cefa3d662dbdc
ChivaSdev <melvinlucchini357@gmail.com>
2020-08-21 16:34:25 +0200

    Added the possiblity to load market datas between a certain date

--- trading_bot/apis/alpha_vantage_api.py
+++ trading_bot/apis/alpha_vantage_api.py
@@ -1,5 +1,7 @@
 from alpha_vantage.timeseries import TimeSeries
+import numpy as np
 import pandas as pd
+from time import sleep
 
 from trading_bot.settings import ALPHA_VANTAGE_API_KEY
 from .base_api import BaseAPI
@@ -10,15 +12,46 @@ class AlphaVantage(BaseAPI):
         BaseAPI.__init__(self, None)
 
     def load_data(self, start_date: str, end_date: str, symbol: str) -> pd.DataFrame:
-        ts = TimeSeries(key=ALPHA_VANTAGE_API_KEY, output_format="pandas")
-        data, meta_data = ts.get_intraday(symbol, interval="1min")
-        self.df = data
-        dataDict = {
-            "1. open": "Open",
-            "2. high": "High",
-            "3. low": "Low",
-            "4. close": "Close",
-            "5. volume": "Volume"
-        }
-        self.rename_column_name(dataDict)
-        return data
+        """ This method will load datas from any market by requesting Alpha_Vantage website.
+
+        Args:
+            start_date (str): contains the starting date 
+            end_date (str): contains the ending date 
+            symbol (str): name of a Corp. 
+
+        Returns:
+            pd.DataFrame: contains all the datas that we want
+        """
+        i = 0
+        while i <= 5:
+
+            ts = TimeSeries(key=ALPHA_VANTAGE_API_KEY, output_format="pandas")
+            data, meta_data = ts.get_intraday(symbol, interval="1min", outputsize="full")
+            self.df = data
+            dataDict = {
+                "1. open": "Open",
+                "2. high": "High",
+                "3. low": "Low",
+                "4. close": "Close",
+                "5. volume": "Volume"
+            }
+            self.rename_column_name(dataDict)
+
+            # expected_output = self.df[self.df.index >= start_date]
+            # expected_output = expected_output[expected_output.index <= end_date]
+
+            expected_output = self.df[np.logical_and(self.df.index >= start_date, self.df.index <= end_date)]
+            time.sleep(60)
+            i += i
+
+        return expected_output
+
+    def update_datas(self, datas: dict) -> pd.DataFrame: 
+        pass
+        
+        
+
+
+        
+
+
